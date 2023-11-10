8cd204ec2c0d190533e7c10b5e258f5006573e69
mauri2012 <lezanamauricio86@gmail.com>
2023-11-10 20:14:58 -0300

    correccion de la correccion

--- ProyectoIntegradorTaller/logica/ReporteLogica.cs
+++ ProyectoIntegradorTaller/logica/ReporteLogica.cs
@@ -83,7 +83,7 @@ namespace ProyectoIntegradorTaller.logica
                              join dias_semana in db.dias_semana on reserva.id_dia equals dias_semana.id_dias
                              join Periodo in db.Periodo on  reserva.id_periodo equals Periodo.id_periodo
                             join usuario in db.usuario on reserva.id_usuario equals usuario.id_usuario 
-                            where aula.activa == "SI" && aula.id_aula == id_aula && reserva.activo=="SI" && usuario.idtipousuario==4
+                            where aula.activa == "SI" && aula.id_aula == id_aula && reserva.activo=="SI" && usuario.id_tipoUsuario==4
                              && ((((Periodo.fecha_hasta <= periodoElejido.fecha_hasta && Periodo.fecha_desde >= periodoElejido.fecha_desde) || Periodo.id_periodo == 4) && periodoElejido.id_periodo == 1) ||
                                    (((Periodo.fecha_hasta >= periodoElejido.fecha_hasta && Periodo.fecha_desde <= periodoElejido.fecha_desde) || Periodo.id_periodo == 4) && (periodoElejido.id_periodo == 7))
                                    || ((Periodo.fecha_desde == periodoElejido.fecha_desde || Periodo.id_periodo == 4) && (periodoElejido.id_periodo == 6 || periodoElejido.id_periodo == 8))
@@ -116,7 +116,7 @@ namespace ProyectoIntegradorTaller.logica
                             join usuario in db.usuario on reserva.id_usuario equals usuario.id_usuario
                             join aula in db.aula on reserva.id_aula equals aula.id_aula
                             join Periodo in db.Periodo on reserva.id_periodo equals Periodo.id_periodo
-                            where  aula.activa=="SI"  && aula.id_aula == id_aula &&  reserva.activo=="SI" && && usuario.idtipousuario==4&& ((((Periodo.fecha_hasta <= periodoElejido.fecha_hasta && Periodo.fecha_desde >= periodoElejido.fecha_desde) || Periodo.id_periodo == 4) && periodoElejido.id_periodo == 1) ||
+                            where  aula.activa=="SI"  && aula.id_aula == id_aula && usuario.id_tipoUsuario==4 &&  reserva.activo=="SI" && (((((Periodo.fecha_hasta <= periodoElejido.fecha_hasta && Periodo.fecha_desde >= periodoElejido.fecha_desde) || Periodo.id_periodo == 4) && periodoElejido.id_periodo == 1) ||
                                    (((Periodo.fecha_hasta >= periodoElejido.fecha_hasta && Periodo.fecha_desde <= periodoElejido.fecha_desde) || Periodo.id_periodo == 4) && (periodoElejido.id_periodo == 7))
                                    || ((Periodo.fecha_desde == periodoElejido.fecha_desde || Periodo.id_periodo == 4) && (periodoElejido.id_periodo == 6 || periodoElejido.id_periodo == 8))
                                    || ((Periodo.fecha_desde >= periodoElejido.fecha_desde || Periodo.id_periodo == 4) && (periodoElejido.id_periodo == 2))
@@ -126,7 +126,7 @@ namespace ProyectoIntegradorTaller.logica
                                       || Periodo.id_periodo == 2 || Periodo.id_periodo == 5 || Periodo.id_periodo == 8
                                       || Periodo.id_periodo == 10 || Periodo.id_periodo == 4) && periodoElejido.id_periodo == 4)
                                    || (Periodo.fecha_desde <= periodoElejido.fecha_hasta && periodoElejido.id_periodo == 3)
-                                   || ((Periodo.fecha_desde >= periodoElejido.fecha_desde || periodoElejido.id_periodo == 4) && periodoElejido.id_periodo == 10))
+                                   || ((Periodo.fecha_desde >= periodoElejido.fecha_desde || periodoElejido.id_periodo == 4) && periodoElejido.id_periodo == 10)))
                             group new {reserva, usuario,aula }
                             by new { reserva.id_usuario, usuario.apellido,aula.nombre} into g
                             select new HorarioList
@@ -176,6 +176,7 @@ namespace ProyectoIntegradorTaller.logica
         }
         public static void ListarHoras(Chart chart, int id_aula, string periodo)
         {
+            chart.Series.Clear();
             using (classroom_managerEntities db = new classroom_managerEntities())
             {
                 var periodoElejido = db.Periodo.FirstOrDefault(p => p.periodo_nombre == periodo);
@@ -183,7 +184,7 @@ namespace ProyectoIntegradorTaller.logica
                                join aula in db.aula on reserva.id_aula equals aula.id_aula
                                join horas in db.horas on reserva.id_hora equals horas.id_hora
                                join Periodo in db.Periodo on reserva.id_periodo equals Periodo.id_periodo
-                            where aula.activa == "SI" && aula.id_aula == id_aula && aula.id_aula == id_aula && reserva.activo == "SI" && ((((Periodo.fecha_hasta <= periodoElejido.fecha_hasta && Periodo.fecha_desde >= periodoElejido.fecha_desde) || Periodo.id_periodo == 4) && periodoElejido.id_periodo == 1) ||
+                            where aula.activa == "SI" && aula.id_aula == id_aula  && reserva.activo == "SI" && ((((Periodo.fecha_hasta <= periodoElejido.fecha_hasta && Periodo.fecha_desde >= periodoElejido.fecha_desde) || Periodo.id_periodo == 4) && periodoElejido.id_periodo == 1) ||
                                    (((Periodo.fecha_hasta >= periodoElejido.fecha_hasta && Periodo.fecha_desde <= periodoElejido.fecha_desde) || Periodo.id_periodo == 4) && (periodoElejido.id_periodo == 7))
                                    || ((Periodo.fecha_desde == periodoElejido.fecha_desde || Periodo.id_periodo == 4) && (periodoElejido.id_periodo == 6 || periodoElejido.id_periodo == 8))
                                    || ((Periodo.fecha_desde >= periodoElejido.fecha_desde || Periodo.id_periodo == 4) && (periodoElejido.id_periodo == 2))
